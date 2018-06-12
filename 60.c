f4e767789d97afb7fcd1c057e9c12d15685deadc
yue shuai <wsyueshuai@163.com>
2018-06-12 08:55:05 -0400

    aa

--- 60.c
+++ 60.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 size_t my_strlen(const char* str){
-  size_t count;
-  while(*str++ != 0){
+  assert(str != NULL);
+  
+  const char *tmp = str;
+
+  size_t count = 0;
+  while(*tmp++ != '\0'){
     count++;
   }
   return count;
+}
+
+size_t my_strlen1(const char* str){
+  assert(str != NULL);
+  if(*str == 0){
+    return 0;
+  }else{
+    return my_strlen1(str+1) + 1;
+  }
 }
 
 int main(){
   char as[] = "hello C";
   printf("%ld\n",strlen(as));
   printf("%ld\n",my_strlen(as));
+  printf("%ld\n",my_strlen1(as));
 }
