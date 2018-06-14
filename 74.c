c6fdaeca61aa97624594e03b10cc6ee8b9a322eb
yue shuai <wsyueshuai@163.com>
2018-06-14 22:56:47 +0800

    mem

--- 74.c
+++ 74.c
@@ -1,38 +1,23 @@
 #include <stdio.h>
 #include <memory.h>
 #include <assert.h>
-#include <malloc.h>
 
-void* my_memcpy(void* dest, const void* src, size_t cnt){
+void* my_memmove(void* dest, const void* src, size_t cnt){
 
   assert(NULL != dest && NULL != src);
 
-  char *tmp = dest;
-  const char *st = src;
-
-  size_t bit = st + cnt - tmp;
-  char *tt = NULL;
-  char *tm = NULL;
-  char *ta;
-  if(bit > 0 && tmp > st){
-    tt = (char*)malloc(bit);
-  }
-  ta = tm = tt;
-  
-  while(cnt-- > 0){
-    if(NULL != tt && bit-- > 0){
-      *tt++ = *tmp;
+  char* tmp = dest;
+  const char* st = src;
+  if(tmp > st && st + cnt > tmp){
+    while(cnt-- > 0){
+      *(tmp + cnt) = *(st + cnt);
     }
-    if(st >= (char*)dest && NULL != tt){
-      *tmp++ = *tm++;
-      st++;
-    }
-    else{
+  } else {
+    while(cnt-- > 0){
       *tmp++ = *st++;
     }
-    
   }
-  free(ta);
+
   return dest;
 }
 
@@ -40,8 +25,7 @@ int main(){
   char s1[20] = {'a','b','c','d'};
   char *s2 = "xyzdef";
  
-  //char *s3 = memcpy(s1+1,s1,3);
-  char *s3 = my_memcpy(s1+1,s1,3);
+  char *s3 = my_memmove(s1,s1+1,3);
   printf("s1 = [%s]\n", s1);
   printf("s3 = [%s]\n", s3);
 
@@ -49,7 +33,7 @@ int main(){
   char s11[20] = {'a','b','c','d'};
   char *s22 = "xyzdef";
  
-  char *s33 = memcpy(s11+1,s11,3);
+  char *s33 = memmove(s11,s11+1,3);
   printf("s11 = [%s]\n", s11);
   printf("s33 = [%s]\n", s33);  
   
@@ -58,7 +42,7 @@ int main(){
   int i2[10] = {11,22,33,3};
   int i3[10] = {1};
   int *pi3  = i3;
-  pi3 = (int*)my_memcpy(i2+2,i2,sizeof(int) * 3);
+  pi3 = (int*)my_memmove(i2+2,i2,sizeof(int) * 3);
   for(int i = 0; i < sizeof(i2) / sizeof(int); ++i){
     printf("i1[%d] = %d ",i, i2[i]);
   }
@@ -68,5 +52,5 @@ int main(){
     printf("i3[%d] = %d ",i, pi3[i]);
   }
   printf("\n");
-  
+
 }
