21e522d27699627524c6407968d5dcb9a8c8ed9d
yuebaixiao <wsyueshuai@163.com>
2018-06-15 11:06:06 +0800

    zijieduiqi

--- 80.c
+++ 80.c
@@ -10,17 +10,18 @@ typedef struct B{
   long b;//占用8字节
   char c;//占用1字节
 }B;
-
+typedef struct C{
+  char a;
+  int b;
+  long c;
+}C;
 int main(){
   printf("A size = %ld\n",sizeof(A));
   printf("B size = %ld\n",sizeof(B));
+  printf("C size = %ld\n",sizeof(C));
 
   B B1;
   B1.a = 10;
   B1.b = 12;
   B1.c = 'A';
-  B B2;
-  B2.a = 11;
-  B1.b = 13;
-  B1.c = 'B';
 }
