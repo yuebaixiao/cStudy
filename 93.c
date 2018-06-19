b2493fab8831707e877765b35477ad751a355647
yuebaixiao <wsyueshuai@163.com>
2018-06-19 14:39:55 +0800

    rouxingshuzu

--- 93.c
+++ 93.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 typedef struct Test{
-  
   int a;
   long b;
-  char pc[0];
+  char pc[0]; 
 }Test;
 
 int main(){
-  
+
   int a;
   long b;
-  Test t;
   long c;
   long dd;
   char str[] = "1Hello c Hello c++!";
+  Test t;
+  
   long ee;
+  Test t1;
+  char str1[] = "2Hello c Hello c++!";  
   printf("%s\n",t.pc);
+  printf("%s\n",t1.pc);  
 }
